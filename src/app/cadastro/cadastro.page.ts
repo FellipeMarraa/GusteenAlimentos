4f5d0c63dfe720d00b34580cce4793bdc4e0a674
FellipeMarraa <fellipemarra@hotmail.com>
2021-03-21 12:44:08 -0300

    Tela de login e cadastro criadas e rotas funcionando

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,5 +1,7 @@
 import {Component, Injector} from '@angular/core';
 import {BaseComponent} from "../class/commons-class/base.component";
+import {Cliente} from '../class/cliente';
+import {ClienteService} from '../service/cliente.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,7 +10,12 @@ import {BaseComponent} from "../class/commons-class/base.component";
 })
 export class CadastroPage extends BaseComponent {
 
-  constructor(private injector: Injector) {
+  tipo: boolean;
+  tipoConfirmaSenha: boolean;
+  usuario: Cliente = new Cliente();
+
+  constructor(private injector: Injector,
+              private clienteService: ClienteService) {
     super(injector);
   }
 
@@ -19,4 +26,35 @@ export class CadastroPage extends BaseComponent {
   dismiss() {
     this.navCtrl.navigateRoot("/login")
   }
+
+  exibiOcultarSenha() {
+    this.tipo = !this.tipo;
+  }
+
+  exibirConfirmaSenha(){
+    this.tipoConfirmaSenha = !this.tipoConfirmaSenha;
+  }
+
+  fazerLogin() {
+    //TODO ir no cliente.service.ts metodo logar(this.usuario) que vai me retorna um usuario
+    //Caso o usuario seja retornado deixa seqguir para a tela /home , se nao retornar exibir erro na tela
+
+    //   this.authService.login(this.usuarioAuth).then((usuario) => {
+    //       if (usuario != null) {
+    //         this.appService.setCurrentUser(usuario);
+    //         this.lastUserAuth(usuario);
+    //         this.navCtrl.navigateRoot("/home")
+    //       }
+    //       LoadingUtil.dismiss();
+    //     },
+    //     error => {
+    //       LoadingUtil.dismiss();
+    //       AlertUtil.showError(this.alertCtrl, error.message);
+    //     });
+    // } else {
+    // LoadingUtil.dismiss();
+    // AlertUtil.showError(this.alertCtrl, "Não foi possível conectar-se ao servidor. Por favor, verifique sua conexão.");
+    this.navCtrl.navigateRoot("/login");
+  }
+
 }
--- src/app/class/cliente.ts
+++ src/app/class/cliente.ts
@@ -1,6 +1,8 @@
 export class Cliente {
   id: number;
 
+  cpf: string;
+
   email: string;
 
   senha: string;
