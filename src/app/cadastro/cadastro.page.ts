47ab1bfe41be57ea4e22bb23d3795a0136808844
FellipeMarraa <fellipemarra@hotmail.com>
2021-03-29 17:47:00 -0300

    cadastro.ts

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -2,6 +2,8 @@ import {Component, Injector} from '@angular/core';
 import {BaseComponent} from "../class/commons-class/base.component";
 import {Cliente} from '../class/cliente';
 import {ClienteService} from '../service/cliente.service';
+import {PositionToast, ToastUtil} from '../class/commons-class/toast.util';
+import {ToastType} from '../class/commons-class/toast.type';
 
 @Component({
   selector: 'app-cadastro',
@@ -59,10 +61,14 @@ export class CadastroPage extends BaseComponent {
     this.navCtrl.navigateRoot("/login");
   }
 
-  cadastrar(){
-    this.clienteService.save(this.usuario).subscribe(item => {
-      console.log(item);
-    })
+  cadastrar() {
+    if (this.usuario.cpf != null && this.usuario.email != null && this.usuario.senha != null) {
+      this.clienteService.save(this.usuario).subscribe(item => {
+        console.log(item);
+      })
+    }else{
+      ToastUtil.presentToast(this.toastCtrl, "Todos os campos devem ser preenchidos ", PositionToast.BOTTOM, ToastType.INFO);
   }
+}
 
 }
