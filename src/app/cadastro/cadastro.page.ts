a2c29bc88487d137939b13f5eb333c03fa7afec1
Bruno <brunosantos.engproducao@gmail.com>
2021-03-30 18:37:01 -0300

    senhas iguais validadas

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -16,6 +16,7 @@ export class CadastroPage extends BaseComponent {
   tipo: boolean;
   tipoConfirmaSenha: boolean;
   usuario: Cliente = new Cliente();
+  confirmaSenha: any;
 
   constructor(private injector: Injector,
               private clienteService: ClienteService,
@@ -43,23 +44,29 @@ export class CadastroPage extends BaseComponent {
 
   cadastrar() {
     if (this.validate(this.usuario)) {
+      if (!(this.usuario.senha == this.confirmaSenha)) {
+        ToastUtil.presentToast(this.toastCtrl, "Senhas diferentes", PositionToast.BOTTOM, ToastType.ERROR);
+      }
+
       this.clienteService.save(this.usuario).subscribe(item => {
-
-        //Se retorna o item é por que salvou se nao deu erro em algum canto
-
-        console.log(item);
+        if (item) {
+          ToastUtil.presentToast(this.toastCtrl, "Usuário cadastrado", PositionToast.BOTTOM, ToastType.SUCCESS);
+          this.navCtrl.navigateRoot('/login');
+        } else {
+          ToastUtil.presentToast(this.toastCtrl, "Email ja cadastrado", PositionToast.BOTTOM, ToastType.SUCCESS);
+        }
+      }, error => {
+        ToastUtil.presentToast(this.toastCtrl, "Erro no servidor", PositionToast.BOTTOM, ToastType.ERROR);
       })
-      ToastUtil.presentToast(this.toastCtrl, "Usuário cadastrado", PositionToast.BOTTOM, ToastType.SUCCESS);
-      this.navCtrl.navigateRoot('/login');
     } else {
-      ToastUtil.presentToast(this.toastCtrl, "Necessário preencher todos os dados", PositionToast.BOTTOM, ToastType.INFO);
+      ToastUtil.presentToast(this.toastCtrl, "Necessário preencher todos os dados", PositionToast.BOTTOM, ToastType.ERROR);
     }
   }
 
   validate(usuario: Cliente) {
     let valido: boolean = true
 
-    if (usuario.cpf != null && usuario.email != null && usuario.senha != null) {
+    if (usuario.cpf == null && usuario.email == null && usuario.senha == null) {
       valido = false
     }
 
@@ -67,4 +74,11 @@ export class CadastroPage extends BaseComponent {
   }
 
 
+  onEmailChange(email: any) {
+
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(email)) {
+      ToastUtil.presentToast(this.toastCtrl, "email invalido", PositionToast.BOTTOM, ToastType.ERROR, 1000);
+    }
+  }
 }
