57ebea1e5981ae4094dba1067d2431ec7038d0ac
FellipeMarraa <fellipemarra@hotmail.com>
2021-04-02 22:25:12 -0300

    LocalStorage

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,10 +1,10 @@
 import {Injectable} from '@angular/core';
 import {Cliente} from '../class/cliente';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
 import {API_CONFIG} from '../config/api.config';
 import {LocalUser} from '../class/local.user';
 import {StorageService} from './storage.service';
+import {CredenciaisDTO} from '../class/dto/credenciais.dto';
 
 @Injectable()
 export class AuthService{
@@ -13,8 +13,8 @@ export class AuthService{
               public storage: StorageService) {
   }
 
-  authenticate(cliente : Cliente){
-    return this.http.post(`${API_CONFIG.baseUrl}/cliente/login`, cliente,
+  authenticate(creds : CredenciaisDTO){
+    return this.http.post(`${API_CONFIG.baseUrl}/cliente/login`, creds,
       {
         observe: 'response',
         responseType: 'text'
@@ -32,7 +32,7 @@ export class AuthService{
   succefullLogin(authorizationValue : string){
 
     let tok = authorizationValue.substring(7);
-    let user : LocalUser = {
+    let user: LocalUser = {
       token : tok
     };
     this.storage.setLocalUser(user);
